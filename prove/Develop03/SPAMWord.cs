e82ed9b4673ec96edd41bad7e48fc145f4cff20e
a-nunez0 <alvaro03030404@gmail.com>
2023-02-07 14:19:13 -0700

    new

--- prove/Develop03/SPAMWord.cs
+++ prove/Develop03/SPAMWord.cs
@@ -5,7 +5,7 @@ Authors: Jeffrey Meldrum
 	Alvaro Nunez
 	Logan Clark
 
-  Date:
+  Date:2/7/2023
 
   Description:
   Holds the words themselves and hides the words for the meorization.
@@ -21,16 +21,40 @@ Authors: Jeffrey Meldrum
 
 public class SPAMWord
 {
+  private String _wordText;
+  private bool _isHidden;
+  
   //TODO implement attributes to hold the text of the word and 
   // the flag to know if it's hidden or not
 
 
   //TODO implement constructor to initialize object
-  public SPAMWord()
+  public SPAMWord(string Word)
   {
+    _wordText = Word;
+    _isHidden = false;
 
   }
 
+  public String ToString()
+  {
+    if (_isHidden == true)
+    {
+      return "___";
+    }
+
+    else
+    {
+      return _wordText;
+    }
+
+  }
+
+  public void changeHiddenState(bool isHidden)
+  {
+    _isHidden = isHidden;
+
+  }
   //TODO implement method to return the words as string or underscores if 
   // it is hidden
 
