a7cceedb67993a4216763943ab2f6b7ada27f67c
AngeloAG <angelo.ag.079@gmail.com>
2023-02-09 12:21:37 -0700

    Implemented program class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -26,6 +26,48 @@ class Program
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello Develop03 World!");
+    SPAMReference spamReference = new SPAMReference("1 Nephi", 3, 7);
+    SPAMScripture spamScripture = new SPAMScripture("And it came to pass that I, Nephi, said unto my father: I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them.", spamReference);
+
+    bool keep_prompting = true;
+
+    while (keep_prompting)
+    {
+      Console.Clear();
+      Console.WriteLine("Scripture memorizer!");
+      SpamTextToConsole("Try to memorize this scripture.\n");
+      String spamScriptureString = spamScripture.ToString();
+      SpamTextToConsole(spamScriptureString);
+      String spamUserInput = SpamGetUserInput();
+
+      spamScripture.HideRandomWord();
+
+      if (spamUserInput == "q")
+      {
+        keep_prompting = false;
+      }
+      if (spamScripture.GetAreAllWordsHidden())
+      {
+        keep_prompting = false;
+      }
+    }
+    Console.Clear();
+    SpamTextToConsole("Thank you for using this program.");
+  }
+
+  static string SpamGetUserInput()
+  {
+    String spamUserInput = Console.ReadLine();
+    return spamUserInput;
+  }
+
+  static void SpamTextToConsole(String spamText)
+  {
+    Console.WriteLine(spamText);
+  }
+
+  static void SpamCloseProgram()
+  {
+    System.Environment.Exit(0);
   }
 }
