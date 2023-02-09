3a9c760de7b9e98aba48cbe3470073e92ee7e34f
AngeloAG <angelo.ag.079@gmail.com>
2023-02-09 12:08:07 -0700

    Removed ToDos

--- prove/Develop03/SPAMWord.cs
+++ prove/Develop03/SPAMWord.cs
@@ -24,11 +24,6 @@ public class SPAMWord
   private String _wordText;
   private bool _isHidden;
 
-  //TODO implement attributes to hold the text of the word and 
-  // the flag to know if it's hidden or not
-
-
-  //TODO implement constructor to initialize object
   public SPAMWord(string Word)
   {
     _wordText = Word;
@@ -55,8 +50,4 @@ public class SPAMWord
     _isHidden = isHidden;
 
   }
-  //TODO implement method to return the words as string or underscores if 
-  // it is hidden
-
-  //TODO implement method to change the hidden state of this word
 }
